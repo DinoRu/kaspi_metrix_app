a0b6f079dc902bdfe98cabd41f77734774d95472
dinoru <diarra.msa@gmail.com>
2025-09-16 12:26:35 +0300

    updated build.graddle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
         create("release") {
             keyAlias = keystoreProperties["keyAlias"] as String?
             keyPassword = keystoreProperties["keyPassword"] as String?
-            storeFile = keystoreProperties["storeFile"]?.let { file("app/$it") }
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
             storePassword = keystoreProperties["storePassword"] as String?
         }
     }
